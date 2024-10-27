415132651199668b3fcd1d6e4b9b0bdca35a4924
Sairam <sairamramavath01@gmail.com>
2024-10-27 22:12:16 +0530

    add get student by id

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,5 +1,8 @@
 package storage
 
+import "github.com/Sairam-04/students-api/internal/types"
+
 type Storage interface {
 	CreateStudent(name, email string, age int) (int64, error)
+	GetStudentByID(id int64) (types.Student, error)
 }
